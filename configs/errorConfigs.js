298fc5fd526776b3612280a790485ea2a540202c
karan9 <karan.srivastava@protonmail.com>
2017-01-12 12:02:31 +0530

    server: create configs to handle seprate systems
    Signed-off-by: karan9 <karan.srivastava@protonmail.com>

---
+++ configs/errorConfigs.js
@@ -0,0 +1,21 @@
+var responses = {
+    messages: {},
+    codes: {} 
+};
+
+responses.codes = {
+    HTTP_OK: 200,
+    HTTP_CONFLICT: 409,
+    HTTP_INTERNAL_ERROR: 500,
+    HTTP_NOT_FOUND: 404
+}
+
+
+responses.messages = {
+    CONFLICT: "Sorry, User already resgistered",
+    INTERNAL_ERROR: "Internal Server Error",
+}
+
+module.exports = {
+    responses: responses
+}
---
+++ configs/sysconfigs.js
@@ -0,0 +1,1 @@
+// This file will contain the system we need to set for our backend
